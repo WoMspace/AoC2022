f4f5e33b32914963b0d962b1888004825134add8
WoMspace <alex0002b@gmail.com>
2022-12-06 02:01:35 +0000

    begin day5

---
+++ Day5/Program.cs
@@ -0,0 +1,25 @@
+﻿string sourceFile = args[0];
+string file = File.ReadAllText(sourceFile);
+
+string initialState = file.Split("\n\n")[0];
+string instructions = file.Split("\n\n")[1];
+
+class Piles
+{
+	public Stack<char>[] piles;
+
+	public void Move(int quantity, int source, int destination)
+	{
+		for(int i = 0; i < quantity; i++)
+		{
+			char transit = piles[source].Pop();
+			piles[destination].Push(transit);
+		}
+	}
+
+	public Piles(string initialstate)
+	{
+		string[] lines = initialstate.Split('\n');
+		
+	}
+}
