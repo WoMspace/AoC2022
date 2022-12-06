275d51b8db7f68e83e9ebaebe0c978da1ddba98e
WoMspace <alex0002b@gmail.com>
2022-12-06 01:15:38 +0000

    begin day4

---
+++ Day4/Program.cs
@@ -0,0 +1,53 @@
+﻿// using Helper.Input;
+
+string   fileName = "";
+string[] file     = File.ReadAllLines(fileName);
+
+// Part 1
+int count = 0;
+foreach(string line in file)
+{
+	var pair = ParsePair(line);
+	if(pair.Elf1.Start.Value <= pair.Elf2.Start.Value && pair.Elf1.End.Value >= pair.Elf2.End.Value)
+	{
+		count++;
+	}
+	else if(pair.Elf2.Start.Value <= pair.Elf1.Start.Value && pair.Elf2.End.Value >= pair.Elf1.End.Value)
+	{
+		count++;
+	}
+}
+Console.WriteLine($"{count} pairs entirely overlap.");
+
+// Part 2
+count = 0;
+foreach(string line in file)
+{
+	var pair = ParsePair(line);
+	if(pair.Elf1.End.Value >= pair.Elf2.Start.Value && pair.Elf1.Start.Value < pair.Elf2.Start.Value)
+	{
+		count++;
+	}
+	else if(pair.Elf2.End.Value >= pair.Elf1.Start.Value && pair.Elf2.Start.Value < pair.Elf1.Start.Value)
+	{
+		count++;
+	}
+}
+Console.WriteLine($"{count} pairs overlap at least slightly.");
+
+Range ParseRange(string elf)
+{
+	// Parse '2-4' into a range
+	string start      = elf.Split('-')[0];
+	string end        = elf.Split('-')[1];
+	int    startIndex = int.Parse(start);
+	int    endIndex   = int.Parse(end);
+	return new Range(startIndex, endIndex);
+}
+
+(Range Elf1, Range Elf2) ParsePair(string pair)
+{
+	Range Elf1 = ParseRange(pair.Split(',')[0]);
+	Range Elf2 = ParseRange(pair.Split(',')[1]);
+	return (Elf1, Elf2);
+}
---
+++ Helper/Input.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace Helper;
+
+public static class Input
+{
+	static string GetFileName(string[] args)
+	{
+		if(args.Length == 0)
+		{
+			Console.WriteLine("Error: Missing required arguments.");
+			ShowHelp();
+			Environment.Exit(1);
+		}
+		else if(args.Contains("--help"))
+		{
+			ShowHelp();
+			Environment.Exit(0);
+		}
+		else if(args.Length == 1)
+		{
+			if(File.Exists(args[0]))
+			{
+				return args[0];
+			}
+			else
+			{
+				Console.WriteLine("Error: File does not exist.");
+				ShowHelp();
+				Environment.Exit(1);
+			}
+		}
+		else if(args.Length > 1) {
+		    Console.WriteLine("Error: Too many arguments.");
+		    ShowHelp();
+		    Environment.Exit(1);
+		}
+		else
+		{
+			throw new UnreachableException();
+		}
+
+		throw new UnreachableException();
+	}
+
+	static void ShowHelp()
+	{
+		string msg = """
+Usage: program <data_file>
+
+data_file		The file containing the data from AoC from this puzzle. Can be the test data or the unique data.
+""";
+		Console.WriteLine(msg);
+	}
+}
